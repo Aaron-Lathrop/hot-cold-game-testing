0d67c6b21c705c73cf2ebec3cc56010c19c8045f
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-11-26 17:49:43 -0700

    add status section smoke test

---
+++ src/components/top-nav.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter});
+
+import {shallow, mount} from 'enzyme';
+
+import TopNav from './top-nav';
+
+describe('<TopNav />', () => {
+    it('Renders without crashing', () => {
+        shallow(<TopNav />);
+    });
+});
